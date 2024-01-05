b5035ef518a1fe959f712faeadb2d27707e792f6
abhinavkrsinghh <22051564@kiit.ac.in>
2024-01-05 20:28:20 +0530

    Intro

---
+++ Intro/1.1.c
@@ -0,0 +1,31 @@
+/* Ques Description : */
+/* (1.1) Write a program to read two numbers and compare the numbers using function call by address.*/
+
+#include <stdio.h>
+void comp(int *x, int *y);
+int main()
+{
+    int a = 0, b = 0;
+    printf("Enter numbers 1 : ");
+    scanf("%d", &a);
+    printf("Enter numbers 2 : ");
+    scanf("%d", &b); // Taking two number inputs
+    comp(&a, &b);    // Passing address of the two variables as arguements for comparison.
+    return 0;
+}
+
+void comp(int *x, int *y)
+{
+    if (*x > *y)
+    {
+        printf("%d is greater than %d", *x, *y);
+    }
+    else if (*x == *y)
+    {
+        printf("Both numbers are equal.");
+    }
+    else
+    {
+        printf("%d is smaller than %d", *x, *y);
+    }
+}
---
+++ Intro/1.2.c
@@ -0,0 +1,55 @@
+/* Ques Description : */
+/*1.2 Write a program to create an array of n elements using dynamic memory allocation.
+Calculate sum of all the prime elements of the array using function and de-allocate the memory of the array after its use.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+int prime(int a);          // Function to check whether number is Prime or not.
+void sum(int *ptr, int n); // Function to calculate the sum of Numbers.
+int main()
+{
+    int n = 0;
+    printf("Enter number of Elements : ");
+    scanf("%d", &n);
+    int *ptr = (int *)malloc(n * sizeof(int));
+    if (ptr == NULL)
+    {
+        printf("Memory allocation failed.\n");
+        return 1;
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("Index : %d : ", i);
+        scanf("%d", &ptr[i]);
+    }
+    sum(ptr, n);
+    free(ptr);
+    return 0;
+}
+
+int prime(int a)
+{
+    for (int i = 2; i < a; i++)
+    {
+        if (a % i == 0)
+        {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+void sum(int *ptr, int n)
+{
+    int sum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (prime(ptr[i]))
+        {
+            sum += ptr[i];
+        }
+    }
+    printf("Sum of Prime Elements : %d", sum);
+}
