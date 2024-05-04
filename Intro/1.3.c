fcb1b528eb5952f405e046ca6400168ce0e008c3
abhinavkrsinghh <22051564@kiit.ac.in>
2024-05-05 05:03:06 +0530

    1.3.c

--- Intro/1.3.c
+++ Intro/1.3.c
@@ -10,32 +10,43 @@ Display the information of employees with gross salary. Use array of structure.
 typedef struct emp
 {
     char name[100];
-    char desig[100];
-    int sal;
-    int hra;
-    int da;
+    char desig[50];
+    float sal;
+    float hra;
+    float da;
 } emp;
-
-void create();
 
 int main()
 {
     int n = 0; // Initialising the number of employees to zero.
     printf("Enter Number of Employees : ");
     scanf("%d", &n);
-    emp rec[100];
+    emp rec[10]; //Initialising the array of employee structure to 10 at max.
     for (int i = 0; i < n; i++)
     {
-        printf("Enter Info for Person : %d", i);
+        printf("Enter Info for Person : %d\n", i);
         printf("Enter Name : ");
         scanf("%s", &rec[i].name);
         printf("Enter Designation : ");
         scanf("%s", &rec[i].desig);
         printf("Enter Salary : ");
-        scanf("%s", &rec[i].sal);
+        scanf("%f", &rec[i].sal);
         printf("Enter HRA : ");
-        scanf("%s", &rec[i].hra);
+        scanf("%f", &rec[i].hra);
         printf("Enter DA : ");
-        scanf("%s", &rec[i].da);
+        scanf("%f", &rec[i].da);
+    }
+
+    printf("\nEmployee Info : \n");
+    for (int i = 0; i < n; i++)
+    {
+        printf("\n");
+        printf("Name : %s\n", rec[i].name);
+        printf("Designation : %s\n", rec[i].desig);
+        printf("Salary : %f\n", rec[i].sal);
+        printf("HRA : %f\n", rec[i].hra);
+        printf("DA : %f\n", rec[i].da);
+        printf("Gross Salary : %f\n", rec[i].sal + (rec[i].sal * rec[i].hra / 100) + (rec[i].sal * rec[i].da / 100));
+        printf("\n");
     }
 }
